f47f31bcae549e3eb04191d7b295cf456070b279
rkotenko <robkotenko@gmail.com>
2015-04-13 14:24:10 -0400

    initial version
    Creates links on English word dictionary pages on SpanishDict.com

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ This little javascript snippet is use to add links to other spanish words on a S
 
 I use SpanishDict.com a lot for my language studies.  One thing that I notice is missing is clickable links to other spanish words that appear when you search for a definition.  Often times, the words is related to, but not quite the same as the one I am looking up. I think it would be nice to be able to click through to those words. It is relatively to find some of the words using the class names, but others might be challenging due to more generic classes.  This will be a continual work in progress, I think.
 
+
 The code snippet is meant to be used within something like GreaseMonkey or Custom Javascript (for Chrome) so that it runs on the domain I want and no others.
+
+The code uses jquery.  This is included via the chrome extension I am using.
+
+
---
+++ script.js
@@ -0,0 +1,44 @@
+// Right now, finds one particular class on the dictionary page for 
+// english words only.  If I go to an spanish word page, words are not
+// links because the english words use a different class.  This will
+// be a next step
+
+// Also, not every word on the page uses the same class.  I have to 
+// research the DOM more to determine what other classes to use
+
+// get all the elements with class of 
+// dictionary-neodict-translation-translation
+
+var palabras = $('.dictionary-neodict-translation-translation');
+
+palabras.each(function() {
+    var palabra,
+        articulo,
+        wordSpan = $(this);
+    // check for a space.  This probably means there is an el or la
+    // we only want to have the word since el or la are not part of 
+    // api url
+    // there is a space in the front. trim it.
+    var parts = wordSpan.text().trim().split(' ');
+
+    // for the moment, I am only going to concern myself with the ones
+    // that are 1 or 2 words long.  Sometimes a longer phrase can appear
+    // but these phrases do not have direct api entries
+    if(parts.length == 1) {
+        palabra = parts[0];
+    } else if(parts.length == 2) {
+        palabra = parts[1];
+        articulo = parts[0];
+    } else {
+        return;
+    }
+
+    // empty and append an anchor
+    var a = $('<a href="' + palabra + '"> ' + wordSpan.text() + '</a>');
+    a.css({
+        "color": "blue",
+        "text-decoration": 'underline'
+    });
+
+    wordSpan.empty().append(a);
+});
